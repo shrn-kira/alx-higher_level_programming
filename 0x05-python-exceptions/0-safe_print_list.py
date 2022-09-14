91bfdca64d304bff80a378853cda50caa747abf5
shrn-kira <shrnkira@gmail.com>
2022-09-14 01:14:20 +0000

    Updating the files

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
 
-
-"""
-* safe_print_list - Function that print 'x' elements of a list.
-*
-* @my_list: This is the input list
-* @x: Represents the number of elements to print
-*
-* Return: The real number of elements printed
-"""
-
 def safe_print_list(my_list=[], x=0):
     elements = 0
-    for i in range(x):
+    for i in range(0, x):
         try:
-            print("{}".format(my_list[i]), end="")
+            print(f'{my_list[i]}', end="")
             elements += 1
-        except IndexError:
-            break
+        except Exception:
+            pass
     print()
-    return elements
+    return count
--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
-"""
-* safe_print_integer - Function that prints an integer with "{:d}".format().
-*
-* @value: value can be any type (integer, string, etc.)
-*
-* Return: True if value has been correctly printed (it means
-*         the value is an integer)
-*         Otherwise, returns False)
-"""
 
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except (TypeError, ValueError):
+        return True
+    except Exception:
         return False
-    return True
