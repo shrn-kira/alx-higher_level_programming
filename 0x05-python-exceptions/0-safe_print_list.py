5434093cdaf63814b5329d743ab8b02e849ed146
shrn-kira <shrnkira@gmail.com>
2022-09-14 01:51:17 +0000

    Updating the task 0 file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     for i in range(0, x):
         try:
             print(f'{my_list[i]}', end="")
-            elements += 1
+            count += 1
         except Exception:
             pass
     print()
