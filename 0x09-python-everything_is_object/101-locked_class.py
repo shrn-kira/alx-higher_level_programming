296f304aec1bac17736aad770cca5cc3a0e88df5
shrn-kira <shrnkira@gmail.com>
2022-09-22 00:54:50 +0000

    Corrections to task files

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""Write a class LockedClass with no class or object attribute"""
+
+
 class LockedClass:
-
-    __slots__ = ['first_name']
-
-    def __init__(self, first_name=''):
-        self.first_name = first_name
+        """ Prevents the user from dynamically creating new instance attributes,
+            except if the new instance attribute is called first_name."""
+            __slots__ = ["first_name"]
