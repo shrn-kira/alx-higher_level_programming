dbda500b0e0b569e4fbc01912fb8bdfdb6d28186
shrn-kira <shrnkira@gmail.com>
2022-09-22 01:07:26 +0000

    Updating the task file

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""This defines a locked class"""
-
-
 class LockedClass:
-        """ Only allows instatiation of an attribute called first_name """
+        __slots__ = ['first_name']
 
-          __slots__ = ["first_name"]
+        def __init__(self, first_name=''):
+            self.first_name = first_name
