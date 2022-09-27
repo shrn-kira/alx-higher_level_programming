4143960e4bd470a25b1e59f9623711ab4e1fd76e
shrn-kira <shrnkira@gmail.com>
2022-09-27 22:59:16 +0000

    Correcting some task files

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,25 +1,37 @@
 #!/usr/bin/python3
-"""Write a class Student that defines a student by"""
+"""Module 10-student.
+Creates a Student class.
+"""
 
 
 class Student:
+    """Class that defines a student.
+    Public attributes:
+        - first_name
+        - last_name
+        - age
+    Public method to_json().
+    """
+
     def __init__(self, first_name, last_name, age):
-        """This function init the class"""
+        """Initializes the Student instance."""
 
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """retrieves a dictionary representation of a Student instance
-        If attrs is a list of strings, only attribute names contained in
-        this list must be retriev"""
-        if attrs is None:
-            return self.__dict__
-        my_dict = {}
-        for i in attrs:
-            if i in self.__dict__ and type(i) is str:
-                my_dict[i] = self.__dict__.get(i)
+        """Retrieves a dictionary representation
+        of a Student instance.
+        Args:
+            - attrs: list of attributes
+        Returns: the dict representation of the instance.
+        """
 
-        return my_dict
-
+        my_dict = dict()
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            for x in attrs:
+                if x in self.__dict__:
+                    my_dict.update({x: self.__dict__[x]})
+            return my_dict
+        return self.__dict__.copy()
--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-"""Write a function that inserts a line of text to a file, after each
-   line containing a specific string """
+"""Module 100-append_after.
+Inserts a line of text to a file,
+after each line containing a specific string.
+"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """Function to add a new string when finished the line if the
-       search_string if find in the current line"""
-    with open(filename, 'r') as f:
-        new_file_lines = []
-        for line in f:
-            new_file_lines.append(line)
-            if line.find(search_string) != -1:
-                new_file_lines.append(new_string)
-    with open(filename, 'w') as f:
-            f.writelines(new_file_lines)
+    """Appends the new_string after
+    the search_string in filename.
+    Args:
+        - filename: name of the file
+        - search_string: string to append after
+        - new_string: new_string to append
+    """
+
+    with open(filename, "r") as f:
+        text = f.readlines()
+
+    with open(filename, "w") as fo:
+        s = ""
+        for line in text:
+            s += line
+            if search_string in line:
+                s += new_string
+        fo.write(s)
