40ac07b0c0aa73732ae7042228016c0574973518
shrn-kira <shrnkira@gmail.com>
2022-12-19 20:53:35 +0000

    Updates

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a post request to the passed URL and displays the body of response
-curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
