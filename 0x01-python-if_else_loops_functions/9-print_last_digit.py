75d5336ed2fa91b4d31f68fbb71dfe1af50d4563
shrn-kira <shrnkira@gmail.com>
2023-08-12 20:51:43 +0000

    Adding the required task files to the created directory

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    if number < 0:
+        number = (number * -1) % 10
+    else:
+        number = number % 10
+
+    print("{:d}".format(number), end="")
+    return number
